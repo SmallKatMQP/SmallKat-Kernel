6cfede53238069ef156a67b313a9d97b428af54b
Alex Tacescu <alextac98@gmail.com>
2018-12-30 00:28:10 -0800

    Adding pendulum test for the library

---
+++ src/main/java/org/smallkat/xl/KatSimulation.java
@@ -0,0 +1,36 @@
+package org.smallkat.xl;
+
+import us.ihmc.simulationconstructionset.SimulationConstructionSet;
+import us.ihmc.simulationconstructionset.SimulationConstructionSetParameters;
+//import us.ihmc.euclid.geometry.Shape3D
+
+public class KatSimulation {
+
+    public static final double DT = 0.001;
+    private SimulationConstructionSet sim;
+
+    public KatSimulation(){
+
+        Kat_XL robot = new Kat_XL("Kat_XL");
+
+        SimulationConstructionSetParameters parameters = new SimulationConstructionSetParameters();
+        parameters.setDataBufferSize(32000);
+
+        sim = new SimulationConstructionSet(robot, parameters);
+        sim.setDT(DT, 20);
+        sim.setGroundVisible(true);
+        sim.setCameraPosition(0, -9.0, 0.6);
+        sim.setCameraFix(0.0, 0.0, 0.70);
+
+        sim.setSimulateDuration(60);
+
+        Thread myThread = new Thread(sim);
+
+        myThread.start();
+    }
+
+    public static void main(String[] args) {
+        new KatSimulation();
+    }
+
+}
